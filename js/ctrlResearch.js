f8e55859ede02c56d8802e32b608153a0fbfee78
IuliaMartinaBulai <33555132+IuliaMartinaBulai@users.noreply.github.com>
2018-03-13 10:23:03 +0100

    Update ctrlResearch.js

--- js/ctrlResearch.js
+++ js/ctrlResearch.js
@@ -149,6 +149,7 @@ app.controller('researchCtrl', function($scope, $rootScope, $routeParams, $locat
     title:    "Home",
     teaser:   true,
     painter:  function(db) {
+              let shortbio = db.publications.concat(db.news);
                 let highlighs = partitionDates(db.news, function(x) {
                   return ["Prize", "Project"].includes(x.type);
                 });
