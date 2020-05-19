16d5bd8e525e34d8282ac9912d1b7387069dd93a
IuliaMartinaBulai <33555132+IuliaMartinaBulai@users.noreply.github.com>
2020-05-19 15:04:48 +0200

    Update ctrlResearch.js

--- js/ctrlResearch.js
+++ js/ctrlResearch.js
@@ -150,10 +150,10 @@ app.controller('researchCtrl', function($scope, $rootScope, $routeParams, $locat
     teaser:   true,
     painter:  function(db) {
                let highlighs = partitionDates(db.news, function(x) {
-                  return ["Prize", "Project", "Grant"].includes(x.type);
+                  return ["Lecture Notes", "Submitted Preprints","Prize", "Project", "Grant"].includes(x.type);
                 });
                 addByKeyword(highlighs, db.news, false, formatOther, function(x) {
-                  return ["Prize", "Project", "Grant"].includes(x.type) ? [getDate(x), getYear(x)] : [];
+                  return ["Lecture Notes", "Submitted Preprints","Prize", "Project", "Grant"].includes(x.type) ? [getDate(x), getYear(x)] : [];
                 }); 
                 let data = db.publications.concat(db.news); 
                 let updates = partitionDates(data, function(x) {
